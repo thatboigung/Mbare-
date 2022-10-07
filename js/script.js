1a2b9436b584a4a137fedad7cfb7bcb71d2dff58
thatboigung <mapfumotapuwa18@gmail.com>
2022-10-07 14:42:27 +0200

    first commit Mbare

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var create_listing_btn = document.getElementById('create-listing');
+create_listing_btn.addEventListener('click' ,function(){
+    document.getElementById('create-type').classList.toggle('create-type-active');
+  
+})
